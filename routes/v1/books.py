99f5acde84435d45a35692261c6ec866383911e0
malepatirahul@hotmail.com <malepatirahul@hotmail.com>
2026-09-07 11:00:13 +0000

    update books for path parameters

--- routes/v1/books.py
+++ routes/v1/books.py
@@ -11,17 +11,23 @@ BOOKS = [
     {"title": "Title Six", "author": "Author Two", "category": "math"},
 ]
 
-@router.get("/books_endpoint")
+# a simple get request to get all the books
+
+@router.get("/books")
 async def read_all_books():
     print("GET /books")
     return BOOKS
 
-# @router.get("/books/title/{book_title}")
-# async def read_book(book_title: str):
-#     for book in BOOKS:
-#         if book["title"].casefold() == book_title.casefold():
-#             return book
-#     return {"data": "Not Found"}
+"""
+path parameters are used to get a specific book by title
+"""
+
+@router.get("/books/title/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book["title"].casefold() == book_title.casefold():
+            return book
+    return {"data": "Not Found"}
 
 # @router.get("/books/by-category")
 # async def read_books_by_category(category: str):
