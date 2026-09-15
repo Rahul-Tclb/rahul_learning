77b1140549bc7b64144fe9762e3b422d72cec04e
malepatirahul@hotmail.com <malepatirahul@hotmail.com>
2026-09-15 15:43:02 +0000

    update code

--- routes/v2/__init__.py
+++ routes/v2/__init__.py
@@ -1,11 +1,11 @@
 """v2 API routes."""
 
 from fastapi import APIRouter
-from .healthcheck import router as healthcheck_router
+# from .healthcheck import router as healthcheck_router
 from .books import router_v2 as books_router_2
 
 router = APIRouter()
 
 # Include endpoint-specific routers
-router.include_router(healthcheck_router)
+# router.include_router(healthcheck_router)
 router.include_router(books_router_2)
