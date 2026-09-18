5ad0b19e3d0ac7dd507018aab6b9d66185c1846c
malepatirahul@hotmail.com <malepatirahul@hotmail.com>
2026-09-18 15:35:47 +0000

    update the code by adding pydantic

--- routes/v2/books.py
+++ routes/v2/books.py
@@ -75,6 +75,7 @@ async def read_all_books():
 
 @router_v2.post('/create-book')
 async def create_book_v2_pydantic(book_request : BookRequest):
-    print("Add Books with pre defined schema and pydantic rules")
-    books_v2.append(book_request)
+    new_book = Book(**book_request.dict())
+    print(type(new_book))
+    books_v2.append(new_book)
     
