7776e85cb9bcf926739aa1bcd85d0649bcb74fde
malepatirahul@hotmail.com <malepatirahul@hotmail.com>
2026-09-18 16:25:48 +0000

    update code

--- routes/v2/books.py
+++ routes/v2/books.py
@@ -28,7 +28,8 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    book_id: Optional[int] =  None
+    # book_id: Optional[int] =  None
+    book_id: int
     title: str = Field(min_length=5 , max_length=10)
     author: str = Field(min_length= 6 , max_length=100)
     description: str = Field(min_length= 6 , max_length=100)
